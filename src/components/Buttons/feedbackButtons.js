82e79677fa6e14e6f3adb736bc9e6fcda891ce82
gritchin-artur <123599698+gritchin-artur@users.noreply.github.com>
2023-07-29 10:36:22 +0200

    done

--- src/components/Buttons/feedbackButtons.js
+++ src/components/Buttons/feedbackButtons.js
@@ -1,10 +1,17 @@
-import css from './feedbackButtons.module.css'
+import css from './feedbackButtons.module.css';
+import propTypes from 'prop-types';
+
 const FeedbackOptions = ({ options, onLeaveFeedback }) => (
-            <div className={css.listButtons}>
+    <div className={css.listButtons}>
         <button className={css.buttons} type="button" onClick={onLeaveFeedback(options.good)}>Good</button>
-                <button className={css.buttons} type="button" onClick={onLeaveFeedback(options.neutral)}>Neutral</button>
-                <button className={css.buttons} type="button" onClick={onLeaveFeedback(options.bad)}>bad</button>
-            </div>
+        <button className={css.buttons} type="button" onClick={onLeaveFeedback(options.neutral)}>Neutral</button>
+        <button className={css.buttons} type="button" onClick={onLeaveFeedback(options.bad)}>bad</button>
+    </div>
 );
+
+FeedbackOptions.propTypes = {
+    options: propTypes.object.isRequired,
+    onLeaveFeedback: propTypes.func.isRequired,
+};
 
 export default FeedbackOptions;
